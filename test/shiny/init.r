5a663706bdb4e302b074619f9db1079325317535
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-05-15 12:40:30 +0200

    debugging shiny

--- test/shiny/init.r
+++ test/shiny/init.r
@@ -6,16 +6,20 @@
 ###########################################################
 # Update this line with the R packages to install:
 
-my_packages = c("shiny")
+install.packages('Rcpp', type = 'source')
+install.packages('httpuv', type = 'source')
+install.packages('shiny', type = 'source')
 
-###########################################################
+# my_packages = c()
 
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
-  }
-  else {
-    cat(paste("Skipping already installed package:", p, "\n"))
-  }
-}
-invisible(sapply(my_packages, install_if_missing))
+# ###########################################################
+
+# install_if_missing = function(p) {
+#   if (p %in% rownames(installed.packages()) == FALSE) {
+#     install.packages(p, dependencies = TRUE)
+#   }
+#   else {
+#     cat(paste("Skipping already installed package:", p, "\n"))
+#   }
+# }
+# invisible(sapply(my_packages, install_if_missing))
